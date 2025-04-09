ba26a6bb543141bd43baa8c4d43eec375aaae16b
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2025-04-09 14:06:37 +0200

    Projektdateien hinzufügen.

---
+++ Animal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace OOP_Polymorphism
+{
+    class Animal
+    {
+        private string name;
+        protected string sound;
+
+        protected Animal_ID_Info animal_ID_Info = new Animal_ID_Info();
+
+        public void SetAnimal_ID_Info(int idNum, string owner)
+        {
+            animal_ID_Info.IDNum = idNum;
+            animal_ID_Info.Owner= owner;
+        }
+
+        public void GetANimal_ID_Info()
+        {
+            Console.WriteLine($"{Name} had the ID of {animal_ID_Info.IDNum} and is owned by {animal_ID_Info.Owner}");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} says {Sound}");
+        }
+
+        public Animal():this("No Name", "No Sound") { }
+
+        public Animal(string name):this(name, "No Sound") { }
+
+        public Animal(string name,  string sound)
+        {
+            Name = name;
+            Sound = sound;
+ 
+        }
+
+        public string Name
+        {
+            get { return name; } 
+            set 
+            {
+                if (!value.Any(char.IsDigit)) {name ="No Name";  }
+                 name = value;
+            }
+           
+
+        }
+
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length>10) { sound = "No Sound"; }
+                sound = value;
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal whiskers = new Animal()
+            {
+                Name = "Wiskers",
+                Sound = "Meow"
+            };
+
+
+            Dog grover = new Dog()
+            {
+                Name = "Grover",
+                Sound = "Woof",
+                Sound2 = "Grrr"
+            };
+
+            grover.Sound = "Wooooof";
+
+            whiskers.MakeSound();
+            grover.MakeSound();
+
+            whiskers.SetAnimal_ID_Info(384, "Gomer");
+            grover.SetAnimal_ID_Info(21343, "Paulina");
+
+
+            whiskers.GetANimal_ID_Info();
+            grover.GetANimal_ID_Info();
+
+
+        }
+    }
+}
